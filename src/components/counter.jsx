04bcd019e20c296a2d656a3aba2f11ac4adb5287
Denis Bugreev <den9.rcq@gmail.com>
2021-08-18 20:25:51 +0300

    added counter increment function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,10 +3,12 @@ import React, {useState} from 'react';
 const Counter = () => {
 	const [count, setCount] = useState(0)
 	const formCount = () => count === 0 ? 'Ноль' : count
+	const getBadgeClasses = () => count === 0 ? 'badge m-2 bg-danger' : 'badge m-2 bg-primary'
+	const handleIncrement = () => setCount(count + 1)
 	return (
 		<>
-			<span className='badge bg-primary m-2'>{formCount()}</span>
-			<button className='btn btn-secondary btn-sm'>Increment</button>
+			<span className={getBadgeClasses()}>{formCount()}</span>
+			<button onClick={handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
 		</>
 	)
 }
