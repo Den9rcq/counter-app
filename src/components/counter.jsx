64f1b58c7ab23d8a75b721a8872eeedeb5912363
Denis Bugreev <den9.rcq@gmail.com>
2021-08-18 20:32:23 +0300

    Create button and change Decrement

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,10 +5,12 @@ const Counter = () => {
 	const formCount = () => count === 0 ? 'Ноль' : count
 	const getBadgeClasses = () => count === 0 ? 'badge m-2 bg-danger' : 'badge m-2 bg-primary'
 	const handleIncrement = () => setCount(count + 1)
+	const handleDecrement = () => setCount(() => count <= 0 ? 0 : count - 1)
 	return (
 		<>
 			<span className={getBadgeClasses()}>{formCount()}</span>
 			<button onClick={handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+			<button onClick={handleDecrement} className='btn btn-secondary btn-sm'>Decrement</button>
 		</>
 	)
 }
