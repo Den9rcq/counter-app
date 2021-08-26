a35a8854c75399bba23b245b751ba69ac3f400ea
Denis Bugreev <den9.rcq@gmail.com>
2021-08-26 15:06:31 +0300

    Moving state into a variable

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,16 +2,22 @@ import React, {useState} from 'react';
 import Counter from "./counter";
 
 const Counters = () => {
-	const [counters, setCounters] = useState([
+	const initialState = [
 		{value: 0, id: 1, name: 'Ложка'},
 		{value: 4, id: 2, name: 'Вилка'},
 		{value: 0, id: 3, name: 'Тарелка'},
 		{value: 0, id: 4, name: 'Стартовый набор минималиста'},
 		{value: 0, id: 5, name: 'Ненужные вещи'},
-	])
+	]
+
+	const [counters, setCounters] = useState(initialState)
+	// Deleting an array element
 	const handleDelete = (counterId) => setCounters(counters.filter(counter => counter.id !== counterId))
+	// Reset state
+	const handleReset = () => setCounters(initialState)
 	return (
 		<div>
+			<button className="btn btn-primary btn-sm m-2" onClick={handleReset}>Сброс</button>
 			{counters.map(counter => (
 				<Counter
 					key={counter.id}
